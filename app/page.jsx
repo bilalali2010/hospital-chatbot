dfd4b495e94fa1f71ff3850f65dede29258f6eb1
bilalali2010 <bilalali0905@gmail.com>
2026-02-01 15:18:24 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,9 @@
-"use client";
+"use client"; // optional here, can be removed
 
-import ChatApp from "./ChatApp";
+import dynamic from "next/dynamic";
+
+// Import ChatApp dynamically, client-side only
+const ChatApp = dynamic(() => import("./ChatApp"), { ssr: false });
 
 export default function Page() {
   return <ChatApp />;
